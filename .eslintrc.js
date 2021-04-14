8e447401b28fb7f231db8a76c3430911fc5f05c9
Meta is Beta <meta.is.beta@gmail.com>
2021-04-14 20:53:51 +0200

    update eslintc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,7 @@ module.exports = {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
   },
+  ignorePatterns: ["/demo/*", "/dist/*"],
   overrides: [
     {
       files: [
