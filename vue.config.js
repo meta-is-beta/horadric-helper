ed1a95216b6b2ecba34da38722737c18b5e5d499
Meta is Beta <meta.is.beta@gmail.com>
2021-05-09 16:26:24 +0200

    WiP

--- vue.config.js
+++ vue.config.js
@@ -10,6 +10,14 @@ module.exports = {
     },
   },
   chainWebpack: (config) => {
+    // Remove comments
+    config.optimization.minimizer("terser").tap((args) => {
+      args[0].terserOptions.output = {
+        ...args[0].terserOptions.output,
+        comments: false,
+      };
+      return args;
+    });
     // TypeScript loader
     config.module.rule("ts");
     config.module.rule("ts").use("ts-loader");
