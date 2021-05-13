eca8c97c8c7960f6b688b3d13a28a93d4331126b
Meta is Beta <meta.is.beta@gmail.com>
2021-05-13 17:56:21 +0200

    WiP

--- src/path-of-exile/tools/get-poe-gems-data.js
+++ src/path-of-exile/tools/get-poe-gems-data.js
@@ -4,25 +4,47 @@ var decode = require("decode-html");
 const fs = require("fs");
 
 const GET_GEM_DATA_QUERY =
-  "https://pathofexile.fandom.com/api.php?action=cargoquery&tables=items,skill_gems,skill&join_on=items._pageID=skill_gems._pageID,items.name=skill.active_skill_name&fields=items.name,skill.html,skill.active_skill_name,skill.skill_id,skill_gems.gem_tags&group_by=items.name&format=json&where=skill_id!=%22%22&limit=500";
+  "https://pathofexile.fandom.com/api.php?action=cargoquery&tables=items,skill_gems,skill&join_on=skill.active_skill_name=items.name,items._pageID=skill_gems._pageID&fields=items.name,skill.html,skill.active_skill_name,skill.skill_id,skill_gems.gem_tags&format=json&order_by=name";
 const GEMS_ICONS_CDN_BASE_URL =
   "https://web.poecdn.com/image/Art/2DItems/Gems/";
 
 const getRawDataJson = async () => {
-  const response = await fetch(GET_GEM_DATA_QUERY);
-  return (await response.json()).cargoquery;
+  const pageSize = 500;
+  let page = 0;
+  let response = {};
+  let requestGemData = [];
+  let totalGemData = [];
+
+  do {
+    response = await fetch(
+      `${GET_GEM_DATA_QUERY}&limit=${pageSize}&&offset=${page * pageSize}`
+    );
+    requestGemData = (await response.json()).cargoquery;
+    if (requestGemData.length > 0) {
+      totalGemData = totalGemData.concat(requestGemData);
+    }
+    page++;
+  } while (requestGemData.length > 0);
+  return totalGemData;
 };
 
 const mapData = (rawData) => {
   return rawData
     .map((entry) => {
       const gemData = entry.title;
-      const name = gemData["active skill name"];
+      const $ = cheerio.load(decode(gemData.html));
+
+      const name =
+        gemData["active skill name"] ||
+        $("tr")
+          .filter((_, el) => $(el).find("th").text() == "Skill Id")
+          .find("td")
+          .text()
+          .match(/\[\[([A-z ]+)|[A-z]+\]\]/)[1];
       if (!name) {
         return;
       }
       const skillId = gemData["skill id"];
-      const $ = cheerio.load(decode(gemData.html));
       const propSections = $("tr").filter(
         (_, e) =>
           $(e).find("th").length &&
@@ -46,11 +68,19 @@ const mapData = (rawData) => {
                 (_, e) => `${$(e).find("th").text()}: ${$(e).find("td").text()}`
               )
             ),
-          ],
+          ].filter((p) => p != ""),
         },
         iconSrc: skillId
-          ? skillId.includes("Support")
-            ? `${GEMS_ICONS_CDN_BASE_URL}passives/${skillId}.png`
+          ? skillId.includes("Support") && skillId.includes("Plus")
+            ? `${GEMS_ICONS_CDN_BASE_URL}Support/SupportPlus/${skillId.replace(
+                "Support",
+                ""
+              )}.png`
+            : skillId.includes("Support")
+            ? `${GEMS_ICONS_CDN_BASE_URL}Support/${skillId.replace(
+                "Support",
+                ""
+              )}.png`
             : `${GEMS_ICONS_CDN_BASE_URL}${skillId}.png`
           : "",
       };
