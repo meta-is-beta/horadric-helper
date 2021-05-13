8d99bc4d1c0e8919d09e0852acae8c8ace539005
Meta is Beta <meta.is.beta@gmail.com>
2021-05-13 20:16:55 +0200

    WiP

--- src/path-of-exile/components/item/poe-item.vue
+++ src/path-of-exile/components/item/poe-item.vue
@@ -16,7 +16,7 @@
       <v-popover
         trigger="hover click"
         placement="auto"
-        :offset="10"
+        :offset="20"
         hideOnTargetClick
         :popoverClass="popoverClassesComputed"
         :popoverWrapperClass="popoverWrapperClasses"
