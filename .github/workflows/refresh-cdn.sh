7293e2f2daeda416ba53a4a6ee95bbe61eb5773e
Meta is Beta <meta.is.beta@gmail.com>
2021-05-27 21:01:22 +0200

    Added CDN refresh after release

--- .github/workflows/refresh-cdn.sh
+++ .github/workflows/refresh-cdn.sh
@@ -37,4 +37,15 @@ jobs:
           commit_message: Main release
 
       - name: Purge CDN for 'latest' tag
-        run: ./.github/workflows/refresh-cdn.sh
+        run: curl -X POST \
+              http://purge.jsdelivr.net/ \
+              -H 'cache-control: no-cache' \
+              -H 'content-type: application/json' \
+              -d '{
+              "path": [
+                  "/gh/meta-is-beta/horadric-helper@latest/dist/poe/poe-item-showcase.umd.js",
+                  "/gh/meta-is-beta/horadric-helper@latest/dist/poe/poe-item-showcase.umd.min.js",
+                  "/gh/meta-is-beta/horadric-helper@latest/dist/poe/poe-item-showcase.common.js",
+                  "/gh/meta-is-beta/horadric-helper@latest/dist/poe/poe-item-showcase.css"
+                ]
+              }'
