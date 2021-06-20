675bb54d1e8f74a638f223b643a77f9dbceaaafd
Meta is Beta <82339211+meta-is-beta@users.noreply.github.com>
2021-06-20 23:18:14 +0200

    Pob item data recognition (#17)
    Feature: PoB item data recognition

--- src/path-of-exile/types.d.ts
+++ src/path-of-exile/types.d.ts
@@ -5,54 +5,66 @@ type PoeConfig = {
   iconUrl: string | null;
 };
 
+type PoeItemRarity = "normal" | "rare" | "magic" | "unique" | "gem" | "";
+type PoeItemType =
+  | "Equipment"
+  | "Gem"
+  | "Jewel"
+  | "Flask"
+  | "Currency"
+  | "Map"
+  | "";
+type PoeItemInfluence =
+  | "crusader"
+  | "warlord"
+  | "hunter"
+  | "redeemer"
+  | "elder"
+  | "shaper"
+  | "replica"
+  | "";
+type PoeItemStatus = "corrupted" | "mirrored" | "split";
+type PoePassiveType =
+  | "basic"
+  | "notable"
+  | "keystone"
+  | "ascendancy basic"
+  | "ascendancy notable";
+
+type PoeItemSections = {
+  itemLevel: String | undefined;
+  requirements: String[] | undefined;
+  properties: String[] | undefined;
+  enchants: String[] | undefined;
+  implicits: String[] | undefined;
+  modifiers: String[] | undefined;
+  gemDescription: String[] | undefined;
+  statuses: PoeItemStatus[];
+};
+
+type PoePassiveSections = {
+  description: String[];
+};
+
 type PoeItem = {
-  rarity: "normal" | "rare" | "magic" | "unique" | "gem" | "";
+  rarity: PoeItemRarity;
   class: String | undefined;
-  type: "Equipment" | "Gem" | "Jewel" | "Flask" | "Currency" | "Map" | "";
+  type: PoeItemType;
   name: String | undefined;
   baseName: String | undefined;
-  influences: (
-    | "crusader"
-    | "warlord"
-    | "hunter"
-    | "redeemer"
-    | "elder"
-    | "shaper"
-    | "replica"
-    | ""
-  )[];
+  influences: PoeItemInfluence[];
   sockets: String[] | undefined;
-  sections:
-    | {
-        itemLevel: String | undefined;
-        requirements: String[] | undefined;
-        properties: String[] | undefined;
-        enchants: String[] | undefined;
-        implicits: String[] | undefined;
-        modifiers: String[] | undefined;
-        gemDescription: String[] | undefined;
-        statuses: ("corrupted" | "mirrored" | "split")[];
-      }
-    | {};
+  sections: PoeItemSections | {};
+};
+
+type PoePassive = {
+  name: String;
+  type: PoePassiveType;
+  sections: PoePassiveSections | {};
 };
 
 type PoeItemDataSection = {
   name: String;
   lines: String[];
   index: number;
-};
-
-type PoePassive = {
-  name: String;
-  type:
-    | "basic"
-    | "notable"
-    | "keystone"
-    | "ascendancy basic"
-    | "ascendancy notable";
-  sections:
-    | {
-        description: String[];
-      }
-    | {};
 };
